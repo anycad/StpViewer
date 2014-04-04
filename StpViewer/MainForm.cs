0d08f211d6eba62258097528e733e63bacab2885
anycad <anycad@anycad.net>
2014-04-04 12:37:03 +0800

    Add StpViewer

---
+++ StpViewer/MainForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AnyCAD.Platform;
+namespace StpViewer
+{
+    public partial class MainForm : Form
+    {
+        private AnyCAD.Presentation.RenderWindow3d renderView = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.renderView = new AnyCAD.Presentation.RenderWindow3d();
+            this.renderView.Location = new System.Drawing.Point(0, 0);
+            this.renderView.Size = this.Size;
+            this.renderView.TabIndex = 1;
+            this.splitContainer1.Panel2.Controls.Add(this.renderView);
+
+            this.renderView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.OnRenderWindow_MouseClick);
+        }
+
+        private void OnRenderWindow_MouseClick(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (renderView != null)
+                renderView.Size = this.splitContainer1.Panel2.Size;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.renderView.View3d.ShowCoordinateAxis(true);
+            renderView.ExecuteCommand("ShadeWithEdgeMode");
+            this.renderView.RequestDraw();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "STEP File(*.stp;*.step)|*.stp;*.step|All Files(*.*)|*.*";
+
+            if (DialogResult.OK == dlg.ShowDialog())
+            {
+                this.treeViewStp.Nodes.Clear();
+                this.renderView.ClearScene();
+
+                StpBrower browser = new StpBrower(this.treeViewStp, this.renderView);
+                browser.ReadFile(dlg.FileName);
+            }
+
+            renderView.View3d.FitAll();
+        }
+
+        private void treeViewStp_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+           
+        }
+    }
+
+
+}
---
+++ StpViewer/StpBrower.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AnyCAD.Platform;
+using AnyCAD.Presentation;
+
+namespace StpViewer
+{
+    class StpBrower : AnyCAD.Platform.ITopoShapeReaderContext
+    {
+        private System.Windows.Forms.TreeView treeView = null;
+        private AnyCAD.Presentation.RenderWindow3d renderView = null;
+        private Stack<System.Windows.Forms.TreeNode> nodeStack = new Stack<System.Windows.Forms.TreeNode>();
+        private int nShapeCount = 100;
+
+        public StpBrower(System.Windows.Forms.TreeView _treeView, AnyCAD.Presentation.RenderWindow3d _renderView)
+        {
+            treeView = _treeView;
+            renderView = _renderView;
+        }
+
+        public bool ReadFile(String fileName)
+        {
+            AnyCAD.Exchange.StepReader reader = new AnyCAD.Exchange.StepReader();
+            return reader.Read(fileName, this);
+        }
+
+        public override void OnBeginGroup(String name)
+        {
+            if (name.Length == 0)
+            {
+                name = "<UNKNOWN>";
+            }
+
+            if (nodeStack.Count == 0)
+            {
+                System.Windows.Forms.TreeNode node = treeView.Nodes.Add(name);
+                nodeStack.Push(node);
+            }
+            else
+            {
+                nodeStack.Push(nodeStack.Peek().Nodes.Add(name));
+            }
+        }
+
+        public override void OnEndGroup()
+        {
+            nodeStack.Pop();
+        }
+
+        public override void OnCompound(TopoShape shape)
+        {
+            ++nShapeCount;
+            nodeStack.Peek().Nodes.Add(String.Format("{0}", nShapeCount), "Assembly");
+            renderView.ShowGeometry(shape, nShapeCount);
+        }
+
+        public override void OnSolid(TopoShape shape)
+        {
+            ++nShapeCount;
+            nodeStack.Peek().Nodes.Add(String.Format("{0}", nShapeCount), "Solid");
+            renderView.ShowGeometry(shape, nShapeCount);
+        }
+
+        public override void OnShell(TopoShape shape)
+        {
+            ++nShapeCount;
+            nodeStack.Peek().Nodes.Add(String.Format("{0}", nShapeCount), "Shell");
+            renderView.ShowGeometry(shape, nShapeCount);
+        }
+
+        public override void OnFace(TopoShape shape)
+        {
+            ++nShapeCount;
+            nodeStack.Peek().Nodes.Add(String.Format("{0}", nShapeCount), "Face");
+            renderView.ShowGeometry(shape, nShapeCount);
+        }
+    }
+}
